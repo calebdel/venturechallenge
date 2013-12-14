231deebb66bf96192af34af9ea2aa9227e5f5589
thejoshlamb <joshlamb@gmail.com>
2013-12-14 11:09:26 -0500

    cleaning up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def admin_new
+  end
 
   def create
     if params[:url]
@@ -22,7 +24,13 @@ class SessionsController < ApplicationController
     end
   end
 
-  def admin_new
+  
+
+  def destroy
+    session[:shopify] = nil
+    session[:user_id] = nil
+    flash[:notice] = "Successfully logged out."
+    redirect_to root_path
   end
 
   def authorize
@@ -42,12 +50,6 @@ class SessionsController < ApplicationController
 
   private
 
-
-    
-  
-
-
-  
   def sanitize_shop_param(params)
     return unless params[:url].present?
     name = params[:url].to_s.strip
@@ -58,80 +60,14 @@ class SessionsController < ApplicationController
     u.host.ends_with?("myshopify.com") ? u.host : nil
   end
 
+  def initialize_webhooks
+    topics = ["orders/create"]
+    topics.each do |topic|
+      webhook = ShopifyAPI::Webhook.create(format: "json", topic: topic, address: "#{ENV['HOST_URL']}/webhooks/#{topic}")
+      # raise "Webhook invalid: (#{topic}) #{webhook.errors}" unless webhook.valid?
+    end
+  end
+
 end
-  
-#   def new
-#     authenticate_store if params[:shop].present?
-#   end
-
-#   def create
-#     authenticate_store
-    
-#   end
-  
-#   def show
-#     if response = request.env['omniauth.auth']
-#       sess = ShopifyAPI::Session.new(params[:shop], response['credentials']['token'])
-#       session[:shopify] = sess  
-#       ShopifyAPI::Base.activate_session(sess)
-      
-#       initialize_webhooks
-      
-#       if User.find_or_create_by_name3(sess.url, access_token: sess.token)
-    
-
-
-#       # current_user = User.find_or_create_by_email(ShopifyAPI::Shop.current_store.email) #you get the idea
-#       # current_user.password?
-#       # give him a password if he dont got one
-      
-#         flash[:notice] = "Logged in"
-#         redirect_back_or_to root_path
-
-#       else
-#         flash[:error] = "Could not log in to Shopify store."
-#         redirect_to :action => 'new'
-#       end
-
-#     else
-#       flash[:error] = "Could not log in to Shopify store."
-#       redirect_to :action => 'new'
-#     end
-#   end
-  
-#   def destroy
-#     session[:shopify] = nil
-#     flash[:notice] = "Successfully logged out."
-    
-#     redirect_to :action => 'new'
-#   end
-  
-#   protected
-
-#   def initialize_webhooks
-#     topics = ["orders/create"]
-#     topics.each do |topic|
-#       webhook = ShopifyAPI::Webhook.create(format: "json", topic: topic, address: "#{ENV['HOST_URL']}/webhooks/#{topic}")
-#       # raise "Webhook invalid: (#{topic}) #{webhook.errors}" unless webhook.valid?
-#     end
-#   end
-  
-#   def authenticate_store
-#     if shop_name = sanitize_shop_param(params)
-#       redirect_to "/auth/shopify?shop=#{shop_name}"
-#     else
-#       redirect_to return_address
-#     end
-#   end
 
   
-#   def sanitize_shop_param(params)
-#     return unless params[:shop].present?
-#     name = params[:shop].to_s.strip
-#     name += '.myshopify.com' if !name.include?("myshopify.com") && !name.include?(".")
-#     name.sub('https://', '').sub('http://', '')
-
-#     u = URI("http://#{name}")
-#     u.host.ends_with?("myshopify.com") ? u.host : nil
-#   end
-# end
