a91bb005c424435aa4e1a30d3f2492bb7627cd96
thejoshlamb <joshlamb@gmail.com>
2013-12-15 17:41:54 -0500

    added validations for dates

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,23 @@
 class League < ActiveRecord::Base
 	belongs_to :user
 	has_many :stores
-  
+
+	validate :start_date_not_in_the_past
+
+	validate :end_date_not_before_start_date
+
+
+	def start_date_not_in_the_past
+		if start_date < Time.now
+			errors.add(:end_date, "Start Date must be in the future")
+		end
+	end
+
+	def end_date_not_before_start_date
+		if start_date > end_date
+     errors.add(:end_date, "End date cannot be before Start Date")
+ 		end
+ 	end
+
+
 end
--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,7 @@
 class Level < ActiveRecord::Base
 belongs_to :badge  
 belongs_to :store  
+
+def 
+
 end
