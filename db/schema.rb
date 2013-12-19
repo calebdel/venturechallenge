d252570f9a6c9d1caaa9bbe1ccaf9ba70ed5fba5
apoon- <anson.os.poon@gmail.com>
2013-12-18 22:19:57 -0500

    fixed admin resource link

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20131219004839) do
-=======
-ActiveRecord::Schema.define(version: 20131218221113) do
->>>>>>> posts
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "assets", force: true do |t|
-    t.string   "url"
-    t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "badge_id"
-  end
 
   create_table "badge_assets", force: true do |t|
     t.string   "url"
