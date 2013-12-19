38f68d7f1f748a88d4ebe0a74aa13d68db85ac4c
calebdel <delbegio@gmail.com>
2013-12-18 18:44:19 -0800

    got gid of sessions css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,6 @@ Venturechallenge::Application.configure do
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
-  config.serve_static_assets = true
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
