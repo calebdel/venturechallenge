70827ba41b81a800385a3b4952713bbe2589cc16
thejoshlamb <joshlamb@gmail.com>
2013-12-19 17:05:59 -0500

    rudimentary badge alert working

--- app/controllers/teamstats_controller.rb
+++ app/controllers/teamstats_controller.rb
@@ -2,6 +2,32 @@ class TeamstatsController < ApplicationController
   def index
   	redirect_to leagues_path unless current_store.league_id
     @store = Store.find_by_user_id(current_user.id)
-  end
 
+    # if the store already has some badges, cache them
+    # if they don't, setup an empty array to compare to
+
+    if @store.badge_array
+    	old_array = @store.badge_array.split(',').map(&:to_i)
+    else
+    	old_array = []
+    end
+
+    # save the current collection of badges to the db
+   	
+   	@store.badge_array = @store.badges.map(&:id).to_s
+		@store.save
+
+		# determine which badges are newly won
+
+   	new_badges = @store.badge_array.split(',').map(&:to_i) - old_array
+
+   	# setup alert
+
+  #  	if new_badges.count == 1
+  #  		flash[:badge] = new_badges.first
+		# elsif new_badges.count > 1
+			flash[:badges] = [1,2,3]
+			# flash[:badges] = new_badges
+		# end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219004839) do
+ActiveRecord::Schema.define(version: 20131219204742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20131219004839) do
     t.integer  "order_count"
     t.integer  "user_id"
     t.integer  "league_id"
+    t.string   "badge_array"
   end
 
   create_table "users", force: true do |t|
