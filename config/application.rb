d1174de885e485816ba9b416d5c22d82dd1814fb
thejoshlamb <joshlamb@gmail.com>
2013-12-18 21:56:12 -0500

    cleaned up manifests in css

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ unless Rails.env == 'production'
     end
 end
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
