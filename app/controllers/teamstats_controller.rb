00fd7714bcadc23d65370c989c8fc427a3802097
calebdel <delbegio@gmail.com>
2013-12-19 20:49:07 -0800

    replaced ranking

--- app/controllers/teamstats_controller.rb
+++ app/controllers/teamstats_controller.rb
@@ -2,7 +2,9 @@ class TeamstatsController < ApplicationController
   def index
   	redirect_to leagues_path unless current_store.league_id
     @store = Store.find_by_user_id(current_user.id)
-
+    social_challenge
+    rankings
+    
     # if the store already has some badges, cache them
     # if they don't, setup an empty array to compare to
 
@@ -31,4 +33,36 @@ class TeamstatsController < ApplicationController
       # flash[:badges] = [1,7,10,11,12]
 		end
   end
+
+  private
+
+  def social_challenge
+    @facebook = Order.where("store_id = #{@store.id}").pluck(:referring_site).grep(/facebook.com/).count
+    @twitter = Order.where("store_id = #{@store.id}").pluck(:referring_site).grep(/twitter.com/).count
+    @pinterest = Order.where("store_id = #{@store.id}").pluck(:referring_site).grep(/pinterest.com/).count
+  end
+
+  def rankings
+     
+    if Point.pluck(:value) == []
+      @rank === 0
+    else
+    p = Point.where("store_id = #{@store.id}").pluck(:value).sum(:value)
+     # setup alert
+ 
+    array = []
+      Store.all.each do |s|
+        points = Point.where("store_id = #{s.id}").pluck(:value).sum(:value)
+        if points == :value
+          points = 0
+        end
+        array << points
+      end
+    array.sort { |x,y| y <=> x }
+    r = Hash[array.map.with_index.to_a]
+    @rank = "#{r[p]} out of #{Store.count}"
+    binding.pry
+    end
+  end
+
 end
