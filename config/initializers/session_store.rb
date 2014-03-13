4579503ddbb41d6b43708655268fcf2e80c7b433
calebdel <delbegio@gmail.com>
2014-03-12 20:26:59 -0700

    redis update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,1 @@
-Venturechallenge::Application.config.session_store :redis_store, :servers => APP_CONFIG['REDISCLOUD_URL'], key: '_venturechallenge_session'
-
+Venturechallenge::Application.config.session_store :redis_store
